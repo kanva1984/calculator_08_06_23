32d901bd359ec6515c647169706a4e83fd6a37a6
kanva1984 <kanva1984@gmail.com>
2023-06-08 18:52:55 +0200

    начало дз

---
+++ src/main/java/com/example/calculator_08_06_23/Calculator080623Application.java
@@ -0,0 +1,13 @@
+package com.example.calculator_08_06_23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator080623Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator080623Application.class, args);
+	}
+
+}
